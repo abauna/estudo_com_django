63a4b78e346d7fb2daef9b18f0d30cdc70987843
abauna <jacksonickolay@gmail.com>
2023-01-12 00:02:38 -0300

    validetion

--- recipes/tests/test_models.py
+++ recipes/tests/test_models.py
@@ -1,2 +1,15 @@
+from django.core.exceptions import ValidationError
+
 from recipes.tests.test_base import Recipe_test_base
+
+
 class RecipesModelTest(Recipe_test_base):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    def test_recipe_title_more_65(self):
+        self.recipe.title = 'a' * 180
+        
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
