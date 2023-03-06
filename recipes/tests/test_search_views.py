d1e92220b64ff1b7298175b2228212d959f1993b
abauna <jacksonickolay@gmail.com>
2023-03-06 17:32:45 -0300

    search

--- recipes/tests/test_search_views.py
+++ recipes/tests/test_search_views.py
@@ -27,3 +27,28 @@ class RecipeSearchviews(Recipe_test_base):
         response = self.client.get(reverse('recipes:search') + '?q=teste')
         self.assertIn('Search for &quot;teste&quot;',
                       response.content.decode('utf-8'))
+
+    def test_recipe_search_can_find(self):
+        title1 = 'This 1'
+        title2 = 'This 2'
+        recipe1 = self.make_recipe(
+            slug="one",
+            title=title1,
+            author={'username': 'one'}
+        )
+        recipe2 = self.make_recipe(
+            slug="vp",
+            title=title2,
+            author={'username': 'vpp'}
+        )
+        sr = reverse('recipes:search')
+        response1 = self.client.get(f'{sr}?q={title1}')
+        response2 = self.client.get(f'{sr}?q={title2}')
+        responsebo = self.client.get(f'{sr}?q=this')
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe2, response2.context['recipes'])
+        self.assertNotIn(recipe1, response2.context['recipes'])
+        self.assertIn(recipe2, responsebo.context['recipes'])
+        self.assertIn(recipe1, responsebo.context['recipes'])
